60d20c2661b94fd79534c85f409bcfb904abac3f
Simon0xzx <simon0xzx@gmail.com>
2017-04-21 07:51:43 -0700

    formatting filenames

---
+++ formating_filename.py
@@ -0,0 +1,12 @@
+import os
+import ipdb; ipdb.set_trace()
+path = "./exam_prob/"
+for filename in os.listdir(path):
+    if not filename.endswith(".pdf"):
+        continue
+    rename_str = filename
+    if filename.startswith("cs70_"):
+        rename_str = rename_str[5:]
+    if filename.endswith("mt1.pdf"):
+        rename_str = rename_str[:-7] + ".pdf"
+    os.rename(path + filename, path + rename_str)
