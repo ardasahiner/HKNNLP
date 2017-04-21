24567feaa97b9eb502d1c5ccf30bd2069b8fb295
Simon0xzx <simon0xzx@gmail.com>
2017-04-21 10:00:59 -0700

    enable parser to parse all files in the directory.

--- pdf_parser.py
+++ pdf_parser.py
@@ -27,32 +27,36 @@ def convert_pdf_to_txt(path):
     retstr.close()
     return text
 
-def parse_problems():
-    path = "./exam_prob/"
+def parse_problems(path):
+    """
+    This function takes in a directory's path and parse all problem files within it, then return a mapping from filename to specific problems.
+    """
     filenames = [filename for filename in os.listdir(path) if filename != '.DS_Store']
-    filename = filenames[0]
-    text = convert_pdf_to_txt(path+filename)
-    lines = [line.strip('\x0c') for line in text.split('\n') if len(line) > 0]
+    mapping = {}
+    for filename in filenames:
+        text = convert_pdf_to_txt(path+filename)
+        lines = [line.strip('\x0c') for line in text.split('\n') if len(line) > 0]
 
-    keywords = ['Problem', 'Section']
-    for i in range(26):
-        keywords.append('{}. '.format(i))
-        keywords.append('({})'.format(chr(ord('a') + i)))
-    sub_keywords = ['pts', 'points']
-    problem, problems = [], []
-    for line in lines:
-        new_prob = False
-        for keyword in keywords:
-            if keyword in line and line.find(keyword) == 0:
-                new_prob = True
-        if new_prob:
-            problems.append(problem)
-            problem = []
-        problem.append(line)
+        keywords = ['Problem', 'Section']
+        for i in range(26):
+            keywords.append('{}. '.format(i))
+            keywords.append('({})'.format(chr(ord('a') + i)))
+        problem, problems = [], []
+        for line in lines:
+            new_prob = False
+            for keyword in keywords:
+                if keyword in line and line.find(keyword) == 0:
+                    new_prob = True
+            if new_prob:
+                problems.append(problem)
+                problem = []
+            problem.append(line)
 
-    problems.append(problem)
-    return problems
+        problems.append(problem)
+        mapping[filename] = problems
+    return mapping
 
 if __name__ == "__main__":
-    problems = parse_problems()
+    path = "./exam_prob/"
+    prob_mapping = parse_problems(path)
     import ipdb; ipdb.set_trace()
